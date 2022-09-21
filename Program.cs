ad8c0749ed900ffa983ca5189a087c0ca82b45bd
Charan Pedapati <hey.charann@gmail.com>
2022-09-21 17:20:10 +0530

    Implemented basic banking functionality with single account

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace BankApplication
                 Console.WriteLine("----------------------------------");
                 Console.WriteLine("");
                 Console.WriteLine("=> Please enter the operation you want to perform:");
-                Console.WriteLine("   1. Withdrawl\n   2. Balance Check\n   3. Add Money (Deposit)");
+                Console.WriteLine("   1. Withdrawl\n   2. Balance Check\n   3. Add Money (Deposit)\n   4. Create new Bank Account");
                 Console.WriteLine("Press (y) to exit...");
                 Console.WriteLine("");
                 Console.WriteLine("----------------------------------");
@@ -32,9 +32,20 @@ namespace BankApplication
                         break;
                     }
 
+                    case "1":
+                    {
+                        bankAccount.WithdrawAmount();
+                        break;
+                    }
+
                     case "2":
                     {
                         bankAccount.GetBankBalance();
+                        break;
+                    }
+                    case "3":
+                    {
+                        bankAccount.DepositAmount();
                         break;
                     }
                 }
@@ -45,27 +56,18 @@ namespace BankApplication
             }while(option!="y");
             
         }
-
-        static int MakeDeposit()
-        {
-            Console.WriteLine("~~~~~ Amount Deposit ~~~~~~");
-            Console.WriteLine("=> Enter the amount you want to add in your account:");
-            // depositAmount = Console.ReadLine();
-            return 0;
-
-        }
-
-    
+ 
     }
 
     public class AccountDetails{
         public string bankAccountNumber="1234";
         public int bankBalance=0;
+        public string userName = "1234";
         public void CreateBankAccount()
         {   
             Console.WriteLine("=> Creating a new bank account for you");
             Console.WriteLine("=> Your Name:");
-            string? name = Console.ReadLine();
+            string name = Console.ReadLine() ?? "";
             Console.WriteLine("=> Your Father Name:");
             string? fatherName = Console.ReadLine();
             Console.WriteLine("=> Your Address:");
@@ -81,6 +83,7 @@ namespace BankApplication
             string accountNumber = dateAndTime.Replace("-","").Replace(" ","").Replace(":","").Substring(0,10);
             this.bankAccountNumber = accountNumber;
             this.bankBalance = Convert.ToInt32(balance);
+            this.userName = name;
             // ---------------------------------------------
             Console.WriteLine("");
             Console.WriteLine("=> Bank Account Created!");
@@ -98,9 +101,35 @@ namespace BankApplication
             
         }
 
+        // This is for Option - 1
+        public void DepositAmount(){
+            Console.WriteLine("Enter the amount you want to deposit in your bank account:");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            this.bankBalance+= amount;
+            Console.WriteLine("Amount Added Successfully!\n New Bank Balance:"+this.bankBalance+" INR");
+        }
+        // This is for option - 2
         public void GetBankBalance()
         {
             Console.WriteLine("\n\n\n Your Current Bank Balance is: "+this.bankBalance+"/-\n\n\n");
         }
+
+        public void WithdrawAmount()
+        {
+            Console.WriteLine("\n\n=> Please enter the amount you want to withdraw:");
+            int withdrawAmount = Convert.ToInt32(Console.ReadLine());
+            if (withdrawAmount>=this.bankBalance)
+            {
+                Console.WriteLine("Insufficient Funds, try with lesser amount!");
+            }
+            else{
+                this.bankBalance-=withdrawAmount;
+                Console.WriteLine("\n=> Amount withdrawn!\n=> Current Balance is: "+this.bankBalance+" INR\n\n");
+            }
+        }
+
+        
     }
+
+
 }
