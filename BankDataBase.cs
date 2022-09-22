e174130110f50b698aaf96ec3e98ab7a308754c0
Charan Pedapati <hey.charann@gmail.com>
2022-09-22 20:43:17 +0530

    Final Version

--- BankDataBase.cs
+++ BankDataBase.cs
@@ -1,8 +1,8 @@
 class BankDataBase{
-    AccountDetails[]? bankAccounts;
     static int index = 0;
+    AccountDetails[] bankAccounts = new AccountDetails[10];
     public void AddBankAccountToDataBase(AccountDetails bankAccount)
-    {
+    {   this.bankAccounts[index] = new AccountDetails();
         this.bankAccounts[index] = bankAccount;
         index+=1;
     }
@@ -12,15 +12,15 @@ class BankDataBase{
         Console.WriteLine("=> Confidential Information. Please Enter PIN to proceed:");
         string? PIN = Console.ReadLine();
         if (PIN=="0000")
-        {   Console.WriteLine("PIN Validated Successfully. Displaying Accounts in Bank Database...\n\n");
+        {   Console.WriteLine("\n=> PIN Validated Successfully. Displaying Accounts in Bank Database...");
             Console.WriteLine("--------------------------------\n");
             int i=0;
-            int length = this.bankAccounts.Count()  ;
-            for (i = 0; i < length; i++)
+            while(this.bankAccounts[i]!= null)
             {
-                Console.WriteLine("Name     :   ",this.bankAccounts[i].userName);
-                Console.WriteLine("Acc No   :   ",this.bankAccounts[i].bankAccountNumber);
-                Console.WriteLine("Balance  :   ",this.bankAccounts[i].bankBalance);
+                Console.WriteLine("Name     :   "+this.bankAccounts[i].userName);
+                Console.WriteLine("Acc No   :   "+this.bankAccounts[i].bankAccountNumber);
+                Console.WriteLine("Balance  :   "+this.bankAccounts[i].bankBalance);
+                i+=1;
                 Console.WriteLine("\n");
 
             }
--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@ namespace BankApplication
             Console.WriteLine("");
             AccountDetails bankAccount = new AccountDetails();
             bankAccount.CreateBankAccount();
+            BankDataBase usersData = new BankDataBase();
+            usersData.AddBankAccountToDataBase(bankAccount);
             Console.WriteLine("\n~~~~~~~~~~~ MAIN MENU ~~~~~~~~~~~~");
             string option = "0";
             do{
@@ -20,7 +22,7 @@ namespace BankApplication
                 Console.WriteLine("----------------------------------");
                 Console.WriteLine("");
                 Console.WriteLine("=> Please enter the operation you want to perform:");
-                Console.WriteLine("   1. Withdrawl\n   2. Balance Check\n   3. Add Money (Deposit)\n   4. Create new Bank Account/n   5. Display all Users(Bank Employee Only)   6. Display all transactions(Needs to be implemented)");
+                Console.WriteLine("   1. Withdrawl\n   2. Balance Check\n   3. Add Money (Deposit)\n   4. Create new Bank Account\n   5. Display all Users(Bank Employee Only)\n   6. Display all transactions(Needs to be implemented)");
                 Console.WriteLine("Press (y) to exit...");
                 Console.WriteLine("");
                 Console.WriteLine("----------------------------------");
@@ -49,10 +51,19 @@ namespace BankApplication
                         break;
                     }
                     // Implement Bank Database Scenario here.
-                    // case "4":
-                    // {
-                    //     BankDatabase
-                    // }
+                    case "4":
+                    {
+                        AccountDetails newBankAccount = new AccountDetails();
+                        newBankAccount.CreateBankAccount();
+                        usersData.AddBankAccountToDataBase(newBankAccount);
+                        break;
+                    }
+
+                    case "5":
+                    {
+                        usersData.DisplayBankAccounts();
+                        break;
+                    }
                 }
                 
 
